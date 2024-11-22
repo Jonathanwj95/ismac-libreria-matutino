e5986c397526c4b19debd6f28ac0eafd836101cc
DM <DM@LAB-SW3-05.ALUMNOS.LOCAL>
2024-11-22 12:30:52 -0500

    Creacion de la capa dao y ejecuciones

--- src/com/distribuida/principal/ComentarioPrincipal.java
+++ src/com/distribuida/principal/ComentarioPrincipal.java
@@ -4,6 +4,7 @@ public class ComentarioPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// clase principal autor para gestionar la base de datos.
 
 	}
 
