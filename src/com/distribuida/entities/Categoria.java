6c5708ae690758289b7ea7364b3bb6089a6281bc
JR <JR@LAB-SW3-05.ALUMNOS.LOCAL>
2024-11-27 13:45:03 -0500

    Tarea 3

--- src/com/distribuida/entities/Categoria.java
+++ src/com/distribuida/entities/Categoria.java
@@ -1,51 +1,54 @@
 package com.distribuida.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Categoria {
-	
-	private int id_categoria;
-	private String categoria;
-	private String descripcion;
-	
-	public Categoria() {}
+    
+    @Id  // Esto marca el campo como la clave primaria de la entidad
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Esto indica que el valor será autogenerado por la base de datos (autoincremento)
+    private int id_categoria;
+    
+    private String categoria;
+    private String descripcion;
+    
+    public Categoria() {}
 
-	public Categoria(int id_categoria, String categoria, String descripcion) {
-		
-		this.id_categoria = id_categoria;
-		this.categoria = categoria;
-		this.descripcion = descripcion;
-	}
+    public Categoria(int id_categoria, String categoria, String descripcion) {
+        this.id_categoria = id_categoria;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+    }
 
-	public int getId_categoria() {
-		return id_categoria;
-	}
+    public int getId_categoria() {
+        return id_categoria;
+    }
 
-	public void setId_categoria(int id_categoria) {
-		this.id_categoria = id_categoria;
-	}
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
 
-	public String getCategoria() {
-		return categoria;
-	}
+    public String getCategoria() {
+        return categoria;
+    }
 
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 
-	public String getDescripcion() {
-		return descripcion;
-	}
+    public String getDescripcion() {
+        return descripcion;
+    }
 
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
-	@Override
-	public String toString() {
-		return "Categoria [id_categoria=" + id_categoria + ", categoria=" + categoria + ", descripcion=" + descripcion
-				+ "]";
-	}
-	
-	
-
-
+    @Override
+    public String toString() {
+        return "Categoria [id_categoria=" + id_categoria + ", categoria=" + categoria + ", descripcion=" + descripcion + "]";
+    }
 }
--- src/com/distribuida/principaldao/ClienteDAOPrincipal.java
+++ src/com/distribuida/principaldao/ClienteDAOPrincipal.java
@@ -15,11 +15,37 @@ public class ClienteDAOPrincipal {
 		
 		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl", ClienteDAO.class);
 		
+		// INSERT
+		Cliente cliente = new  Cliente(0,"1754668711","Juan","Taipe","Quito","09987844640", "jtaipe@correo.com");
+		//clienteDAO.add(cliente);
+		
+		// UPDATE
+		Cliente cliente2 = new  Cliente(39,"1754668712","David","Chuquimarca","Afganistan","09987844643", "afgano@correo.com");
+		//clienteDAO.up(cliente2);
+				
+
+		//findOne
+		try {
+			Cliente cliente3 = clienteDAO.findOne(39);
+			System.out.println(cliente3.toString());
+		}catch(Exception e) {
+			//TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		// DELETE
+		//clienteDAO.del(39);	
+		
+				
+		
+		//findAll
 		List<Cliente> clientes = clienteDAO.findAll();
 		
-		for (Cliente cliente : clientes) {
-			System.out.println(cliente.toString());
+		for (Cliente item : clientes) {
+			System.out.println(item.toString());
 		}
+		
+		
 		
 		context.close();
 	}
